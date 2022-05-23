4c2c84339e617b05424b11ba7601872cd740337d
Hafid <ziti.hafid@gmail.com>
2022-05-23 17:30:37 +0200

    create tweet component

---
+++ src/app/tweet/tweet.component.ts
@@ -0,0 +1,37 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  Output,
+  OnInit,
+  EventEmitter
+} from '@angular/core';
+import { Tweet } from './../model/tweet';
+import { UpdatedTweet } from './../model/updated-tweet';
+
+@Component({
+  selector: 'app-tweet',
+  templateUrl: './tweet.component.html',
+  styleUrls: ['./tweet.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TweetComponent {
+  // Used to know if the tweet is already liked
+  alreadyLiked = false;
+
+  @Input() tweet?: Tweet;
+
+  @Output()
+  // eslint-disable-next-line indent
+  tweetUpdated: EventEmitter<UpdatedTweet> = new EventEmitter();
+
+  constructor() {}
+
+  handleLike(): void {
+    this.tweetUpdated.emit({
+      id: this.tweet!.id,
+      likes: this.alreadyLiked ? --this.tweet!.likes : ++this.tweet!.likes
+    });
+    this.alreadyLiked = !this.alreadyLiked;
+  }
+}
