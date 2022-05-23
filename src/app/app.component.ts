06040f86d6d30fd1651c35aff0880309343d5066
Hafid <ziti.hafid@gmail.com>
2022-05-23 17:33:38 +0200

    update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Tweet } from './model/tweet';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { mockTweets } from './mocks/tweets';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
   title = 'Twitter-Like';
+  tweets: Tweet[] = mockTweets;
+
+  constructor() {}
+
+  handleNewTweet(tweet: Tweet): void {
+    this.tweets = [tweet, ...this.tweets];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TweetComponent } from './tweet/tweet.component';
+import { TweetListComponent } from './tweet-list/tweet-list.component';
+import { NewTweetComponent } from './new-tweet/new-tweet.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LikesPipe } from './pipes/likes.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    TweetComponent,
+    TweetListComponent,
+    NewTweetComponent,
+    LikesPipe
+  ],
+  imports: [BrowserModule, ReactiveFormsModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
