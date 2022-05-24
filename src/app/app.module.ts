6acc92e967c50448297f9b14b5ef4f8e349656d2
Hafid <ziti.hafid@gmail.com>
2022-05-24 14:59:48 +0200

    add tweet store - reducer & actions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AppComponent } from './app.component';
 import { TweetComponent } from './tweet/tweet.component';
 import { TweetListComponent } from './tweet-list/tweet-list.component';
 import { NewTweetComponent } from './new-tweet/new-tweet.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LikesPipe } from './pipes/likes.pipe';
+import { tweetReducer } from './reducers/tweet.reducer';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,16 @@ import { LikesPipe } from './pipes/likes.pipe';
     NewTweetComponent,
     LikesPipe
   ],
-  imports: [BrowserModule, ReactiveFormsModule, FormsModule],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    StoreModule.forRoot({ tweets: tweetReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      autoPause: true // Pauses recording actions and state changes when the extension window is not open
+    })
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
